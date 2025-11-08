52c244fa982d77a75a42596425338a043bb871a3
unknown <adominas01@gmail.com>
2025-11-09 01:55:36 +0200

    added inertia, fixed ((ammo))

--- fun/scripts/killzone.gd
+++ fun/scripts/killzone.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	Engine.time_scale = 0.5
-	body.velocity.y = -200
+	Engine.time_scale = 0.25
+	body.velocity.y = -300
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
-
+	game_manager.deadInputs()
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
+	game_manager.deadInputs()
 	get_tree().reload_current_scene()
--- fun/scripts/player.gd
+++ fun/scripts/player.gd
@@ -6,28 +6,42 @@ const JUMP_VELOCITY = -300
 var hasjumped = 0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var game_manager: Node = %GameManager
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+
 	if is_on_floor():
 		hasjumped = 0
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and hasjumped < 2: 
-		velocity.y = JUMP_VELOCITY
-		hasjumped += 1
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction == 1:
-		animated_sprite.flip_h = false
-	elif direction == -1:
-		animated_sprite.flip_h = true
+	if game_manager.inputs == 1:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and hasjumped < 2: 
+			velocity.y = JUMP_VELOCITY
+			hasjumped += 1
+		
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		if direction == 1:
+			animated_sprite.flip_h = false
+		elif direction == -1:
+			animated_sprite.flip_h = true
+		
+		if direction:
+			if velocity.x >= -1 * SPEED and direction == -1 or velocity.x <= SPEED and direction == 1:
+				if is_on_floor():
+					velocity.x += direction * SPEED * delta * 5
+				elif not is_on_floor():
+					velocity.x += direction * SPEED * delta * 5
+		else:
+			if is_on_floor():
+				velocity.x = move_toward(velocity.x, 0, float(SPEED)/15)
+			elif not is_on_floor():
+				velocity.x = move_toward(velocity.x, 0, float(SPEED)/100)
 	
 	if is_on_floor():
 		if direction == 0:
@@ -37,9 +51,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play ("jump")
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
