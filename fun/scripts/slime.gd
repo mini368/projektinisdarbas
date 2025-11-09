0469e094d211f605abced3102101f393eb3dc2b4
Domas Kavaliauskas <domas.kavaliausku@gmail.com>
2025-11-09 16:44:45 +0200

    Tutorial expanded + slimes now detect ledges

--- fun/scripts/slime.gd
+++ fun/scripts/slime.gd
@@ -6,6 +6,8 @@ var direction = 1
 
 @onready var ray_cast_r: RayCast2D = $RayCastR
 @onready var ray_cast_l: RayCast2D = $RayCastL
+@onready var ray_cast_down_r: RayCast2D = $RayCastDownR
+@onready var ray_cast_down_l: RayCast2D = $RayCastDownL
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +16,12 @@ func _process(delta: float) -> void:
 		direction = -1
 		animated_sprite.flip_h = true
 	if ray_cast_l.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	if not ray_cast_down_r.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if not ray_cast_down_l.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
